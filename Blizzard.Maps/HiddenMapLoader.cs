c9c2ecbf6fd310347fdbc27fddbfc7f7b67328d0
bamsestudio <bamsen@bamsestudio.dk>
2024-07-25 14:21:08 +0200

    added map hidden map support

---
+++ Blizzard.Maps/HiddenMapLoader.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+
+namespace Blizzard.Maps
+{
+    public static class HiddenMapLoader
+    {
+        public static void Init()
+        {
+            foreach (var level in Core.Levels)
+            {
+                level.Options = ELevelOption.Release;
+            }
+            Core.Levels.Add(createLevelData(
+                "MAP_SkullMountains_01",
+                "Skull Mountains",
+                2,
+                2f,
+                2f,
+                2f,
+                2f,
+                2f,
+                1f
+            ));
+            Core.Levels.Add(createLevelData(
+                "MAP_ThePeak_01",
+                "The Peak",
+                2,
+                2f,
+                2f,
+                2f,
+                2f,
+                2f,
+                1f
+            ));
+            Core.Levels.Add(createLevelData(
+                "MAP_TheWall_01",
+                "The Wall",
+                2,
+                2f,
+                2f,
+                2f,
+                2f,
+                2f,
+                1f
+            ));
+        }
+
+        private static LevelData createLevelData(string sceneName, string publicName, int snowfrontCount, float difficulty,
+            float ParameterAccessibility, float ParameterDiversity, float ParameterHomogeneity, float ParameterBuildable, float ParameterObstacles)
+        {
+            return new LevelData
+            {
+                Options = ELevelOption.Release,
+                sceneName = sceneName,
+                publicName = publicName,
+                thumbnail = Core.Levels.First().thumbnail,
+                SnowfrontCount = snowfrontCount,
+                Difficulty = difficulty,
+                ParameterAccessibility = ParameterAccessibility,
+                ParameterDiversity = ParameterDiversity,
+                ParameterHomogeneity = ParameterHomogeneity,
+                ParameterBuildable = ParameterBuildable,
+                ParameterObstacles = ParameterObstacles
+            };
+        }
+    }
+}
